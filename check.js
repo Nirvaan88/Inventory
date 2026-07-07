154a14998735574eecb69b6d994ba506efb1b2f9
tejaswini <tejaswiniathawale13@gmail.com>
2026-07-07 16:24:51 +0530

    Add files via upload

---
+++ check.js
@@ -0,0 +1,11 @@
+const { query, sql } = require('./db.js');
+async function check() {
+  try {
+    const s = await query('SELECT TOP 1 * FROM State');
+    console.log('State columns:', Object.keys(s.recordset[0] || {}));
+    const c = await query('SELECT TOP 1 * FROM City');
+    console.log('City columns:', Object.keys(c.recordset[0] || {}));
+  } catch(e) { console.error('Error:', e.message); }
+  finally { process.exit(0); }
+}
+check();
